f62f976f4b4ae05ad27f29c72273479c08f744d8
Hector Fernandes <hectorhu17@gmail.com>
2025-06-23 09:33:32 -0300

    feat: add login method to save cookie

--- browser.py
+++ browser.py
@@ -1,18 +1,23 @@
-from typing import Self
 from playwright.sync_api import sync_playwright
 
 
 class Browser:
     def __init__(self, headless=False):
         self.playwright = sync_playwright.start()
-        self.browser = self.playwright.chromium.launch(headless=headless)
+        self.browser = self.new_browser(headless=headless)
         self.context = self.browser.new_context()
         self.page = self.context.new_page()
 
-    def goto(self, url, timeout):
+    def goto(self, url, timeout=60000):
         self.page.goto(url=url, timeout=timeout)
         return self.page
+
+    def add_cookie(self, cookie) -> None:
+        self.context.add_cookies(cookie)
 
     def close(self):
         self.browser.close()
         self.playwright.stop()
+
+    def new_browser(self, headless=False):
+        return self.playwright.chromium.launch(headless=headless)
--- linkedin.py
+++ linkedin.py
@@ -1,4 +1,38 @@
+import json
+import os
+
+
 class Linkedin:
     def __init__(self, browser):
         self.BASE_URL = "https://www.linkedin.com/"
+        self.COOKIE_PATH = "cookies/linkedin.json"
         self.browser = browser
+
+    def login(self):
+        if not os.path.exists(self.COOKIE_PATH):
+            self._manual_login()
+
+        self._load_cookie()
+
+    def _manual_login(self) -> None:
+        new_browser = self.browser.new_browser()
+        new_context = new_browser.new_context()
+        page = new_context.new_page()
+        page.goto(f"{self.BASE_URL}/login")
+        print("Please login so that the cookie can be saved!")
+        page.wait_for_timeout(60000)
+        try:
+            self._save_cookie(page)
+        except Exception as e:
+            print(e)
+        new_browser.close()
+
+    def _save_cookie(self, page) -> None:
+        cookie = page.context.cookies()
+        with open(self.COOKIE_PATH, "w") as file:
+            json.dump(cookie, file)
+
+    def _load_cookie(self) -> None:
+        with open(self.COOKIE_PATH, "r") as file:
+            cookie = json.load(file)
+            self.browser.add_cookie(cookie)
