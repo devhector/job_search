e0f18b23ef6b8fbf6e598b644955074a6a10a35c
Hector Fernandes <hectorhu17@gmail.com>
2025-06-24 17:20:44 -0300

    feat: create a bd to dont repeat jobs

---
+++ db.py
@@ -0,0 +1,59 @@
+import sqlite3
+import hashlib
+
+
+class Job_database:
+    def __init__(self, db_path: str = "job.db") -> None:
+        self.conn = sqlite3.connect(db_path)
+        self._create_table()
+
+    def _create_table(self) -> None:
+        self.conn.execute("""
+            CREATE TABLE IF NOT EXISTS jobs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                job_id TEXT UNIQUE,
+                title TEXT,
+                company TEXT,
+                location TEXT,
+                link TEXT
+            )
+        """)
+
+        self.conn.commit()
+
+    def filter_new_jobs(self, jobs: list[dict]) -> list[dict]:
+        cursor = self.conn.cursor()
+
+        new_jobs = []
+        for job in jobs:
+            job["job_id"] = self._generate_job_id(job)
+            cursor.execute(
+                "SELECT 1 FROM jobs WHERE job_id = ?", (job["job_id"],))
+            if cursor.fetchone() is None:
+                new_jobs.append(job)
+        return new_jobs
+
+    def _generate_job_id(self, job: dict) -> str:
+        unique_str = f"{job['title'], job['company'], job['location']}"
+        return hashlib.sha256(unique_str.encode()).hexdigest()
+
+    def save(self, jobs: list[dict]) -> None:
+        cursor = self.conn.cursor()
+
+        for job in jobs:
+            cursor.execute(
+                """
+                INSERT OR IGNORE INTO jobs (job_id, title, company, location, link) VALUES (?, ?, ?, ?, ?)
+            """,
+                (
+                    job["job_id"],
+                    job["title"],
+                    job["company"],
+                    job["location"],
+                    job["link"],
+                ),
+            )
+        self.conn.commit()
+
+    def close(self):
+        self.conn.close()
